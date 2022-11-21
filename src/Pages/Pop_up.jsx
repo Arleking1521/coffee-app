6fe7b45a8eefc62a3c5312ba64b8dca20376b89a
Arleking <Abdukarim600@gmail.com>
2022-11-22 00:12:32 +0600

    Create Pop_up.jsx

---
+++ src/Pages/Pop_up.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
+function Pop_up(props) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <style type='text/css'>
+                {`
+                    *{
+                        z-index: 9999999999;
+                    }
+                `}
+            </style>
+                <button className="btn" onClick={handleShow}>Read More</button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Registration to game {props.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save information
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default Pop_up;
