62f32499c1b12826b9f882063f07ad36f1a13c6b
MAXIM <jeludkovmaxim@gmail.com>
2022-11-21 23:46:39 +0600

    1

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -139,8 +139,9 @@ export default class About extends Component {
 
                 </section>
                 <section className="contact" id="contact">
+                <h1 class="heading"> <span>contact</span> us </h1>
                     <div className="row">
-
+                        
                         <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2907.5737143016854!2d76.92547801535149!3d43.218429679138616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38836f30f7a99069%3A0x7adb88ae41e2c540!2sEsentai%20Mall!5e0!3m2!1sru!2skz!4v1666292588802!5m2!1sru!2skz" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='mapa'></iframe>"
 
                         <form action="">
