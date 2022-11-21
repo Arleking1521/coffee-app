866ef2a061b991a46315b558aebf9aceb4b9db63
Arleking <Abdukarim600@gmail.com>
2022-11-22 02:24:47 +0600

    Update Pop_up.jsx

--- src/Pages/Pop_up.jsx
+++ src/Pages/Pop_up.jsx
@@ -179,11 +179,22 @@ function Pop_up(props) {
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header>
-                    <Modal.Title>Registration to game {props.name}</Modal.Title>
+                    <Modal.Title>{props.name}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
-                        <Form.Label>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Form.Label>
+                        <Form.Label>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                               esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                               esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                                sunt in culpa qui officia deserunt mollit anim id est laborum.</Form.Label>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
